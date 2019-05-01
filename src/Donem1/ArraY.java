7b0115f4d43b052c77a048d4f34c3bfade4a6bb1
MrGodless <47862902+MrGodless@users.noreply.github.com>
2019-05-01 20:24:40 +0300

    asd
    as

--- src/Donem1/ArraY.java
+++
@@ -1,9 +0,0 @@
-package Donem1;
-
-public class ArraY{
-    public static void main(String[] args) {
-   
-        boolean[] ar = {true, false, false, false, true, false};
-        System.out.println(ar[5]);
-}
-}
--- src/Donem1/Lab4Q2.java
+++
@@ -1,2 +0,0 @@
-package Donem1;
-
