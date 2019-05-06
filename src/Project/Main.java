dfa4217b1404a980fc5b2004e62091e8835dcc87
MrGodless <47862902+MrGodless@users.noreply.github.com>
2019-05-06 16:43:37 +0300

    n

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -73,33 +73,43 @@ public class Main {
                             admin.updateLectureInfo();
                             break;
                         case 9:
+                            admin.InstructorRegistration();
+                            break;
+                        case 0:
                             //Admin menüsünden çıkarken bir kere boş geçiyor
                             loopTerminate=false;
                             break;
                         default:
-                            System.out.println("1There is no such option.Try again...");
+                            System.out.println("There is no such option.Try again...");
                             break ;
-                            
+
                     }
                    }
                     break;
-                    
+
                 case 2:
                     instructorDisplayMenu();
                     break;
-                    
+
                 case 3:
                     studentDisplayMenu();
                     break;
-                
+
                 default:
                       System.out.println("2There is no such option please try again...");
                       break;
             }
+<<<<<<< HEAD
             
             }    
         
             
+=======
+
+            }
+
+
+>>>>>>> fcb9b5e971742880f2200c572fb320c70942048f
     }
     public static void adminDisplayMenu()
     {        System.out.println("========================================");
@@ -107,7 +117,7 @@ public class Main {
              System.out.println("========================================");
              System.out.println();
              System.out.println("****************************************");
-             System.out.println("* Here are the admin menu options[1,9] *"); 
+             System.out.println("* Here are the admin menu options[1,9] *");
              System.out.println("* Admin Profile(1)                     *");
              System.out.println("* Student Registration(2)              *");
              System.out.println("* Students List(3)                     *");
@@ -116,15 +126,59 @@ public class Main {
              System.out.println("* Add Lecture(6)                       *");
              System.out.println("* Display Lectures(7)                  *");
              System.out.println("* Update Lecture Information(8)        *");
-             System.out.println("* Exit Admin Menu(9)                   *");
+             System.out.println("* Instructor Registration(9)");
+             System.out.println("* Exit Admin Menu(0)                   *");
              System.out.println("****************************************");
              System.out.println();
              System.out.print("Your choice2:");
-        
+
     }
     public static void mainMenu(){}
-    public static void instructorDisplayMenu(){
-        System.out.println("instructor menu");}
-    public static void studentDisplayMenu(){
-        System.out.println("Student menu");}
-}
+    public static void instructorDisplayMenu() {
+            Scanner s = new Scanner(System.in);
+            System.out.println("========================================");
+            System.out.println("*********Welcome to the Instructor menu**********");
+            System.out.println("========================================");
+            System.out.println();
+            System.out.println("****************************************");
+            System.out.println("----Please enter ID to continue----");
+            long tempID = s.nextLong();
+            for (int i = 0; i < Admin.instructors.size(); i++) {
+                if (Admin.instructors.get(i).getID() == tempID) {
+                    System.out.println("Login Successful");
+                }
+            }}
+            public static void studentDisplayMenu(){
+                Scanner s =new Scanner(System.in);
+                System.out.println("========================================");
+                System.out.println("*********Welcome to the Student menu**********");
+                System.out.println("========================================");
+                System.out.println();
+                System.out.println("****************************************");
+                System.out.println("----Please enter ID to continue----");
+                long tempID = s.nextLong();
+                for (int i = 0; i < Admin.students.size(); i++) {
+                    if (Admin.students.get(i).getID() == tempID) {
+                        System.out.println("Login Successful");
+                        System.out.println("Here are the student menu options[]");
+                        System.out.println("1)Show Grades");
+                        System.out.println("=============================");
+                        if (s.nextInt() == 1) {
+                            System.out.println("Please select the lecture for your grades");
+                            for (int y = 0; y < Admin.lectures.size(); y++) {
+                                System.out.println((y + 1) + ") " + Admin.lectures.get(y).getLectureName());
+                            }
+                            for (int y = 0; y < Admin.lectures.size(); y++) {
+
+                            }
+                        }
+                    }
+                    else {
+                        System.out.println("ID not found!!!");
+                        break;
+                    }
+                }
+
+
+            }
+    }
